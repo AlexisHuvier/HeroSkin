da1c92e68bb7b2e71e6ff288f4b1024a6781b650
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-03-23 02:12:51 +0100

    Beginning of Layers Widget

---
+++ HeroSkin/Elements/Layers.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HeroSkin.Elements
+{
+    /// <summary>
+    /// Logique d'interaction pour Layers.xaml
+    /// </summary>
+    public partial class Layers : UserControl
+    {
+        public MainWindow mainWindow;
+
+        public Layers()
+        {
+            InitializeComponent();
+        }
+        public void SetMainWindow(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+        private void AddLayer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
