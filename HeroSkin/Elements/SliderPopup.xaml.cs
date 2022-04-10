2dcede588bd994e7e60530b6e804e88a196ac3ce
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-04-10 15:59:41 +0200

    Create SliderPopup

---
+++ HeroSkin/Elements/SliderPopup.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HeroSkin.Elements
+{
+    /// <summary>
+    /// Logique d'interaction pour SliderPopup.xaml
+    /// </summary>
+    public partial class SliderPopup : Window
+    {
+        public int Result => Slider == null ? 0 : Convert.ToInt32(Slider.Value);
+
+        public SliderPopup(string text, int value)
+        {
+            InitializeComponent();
+            TextBox.Text = text;
+            Slider.Value = value;
+        }
+
+        private void OnSave(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
